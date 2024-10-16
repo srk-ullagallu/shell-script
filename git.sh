13a5cca3df659fc21da760dbc28c300a3e4627b9
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2024-10-16 18:37:48 +0530

    rectifying errors

--- git.sh
+++ git.sh
@@ -3,6 +3,6 @@
 read -p "Please Enter Commit Message:"  MESSAGE
 read -p "PLease Enter Branch Name:" BRANCH_NAME
 git add .
-git commit -m $MESSAGE
-git push origin $BRANCH_NAME
+git commit -m "$MESSAGE"
+git push origin "$BRANCH_NAME"
 

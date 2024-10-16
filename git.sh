e8c4b80cd8f06c6c7d66c0664c3ee0d3ab2b983d
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2024-10-16 18:42:54 +0530

    added function

--- git.sh
+++ git.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+print_message(){
+    echo "$1"
+}
+
 read -p "Please Enter Commit Message:"  MESSAGE
 read -p "PLease Enter Branch Name:" BRANCH_NAME
 git add .
 git commit -m "$MESSAGE"
 git push origin "$BRANCH_NAME"
 
+print_message Successfully pushing the changes

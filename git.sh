821df8a84152545caa64a43bb72a914c81fdc8f1
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2024-10-16 18:52:29 +0530

    added the script

--- git.sh
+++ git.sh
@@ -4,10 +4,11 @@ print_message(){
     echo "$1"
 }
 
-read -p "Please Enter Commit Message:"  MESSAGE
-read -p "PLease Enter Branch Name:" BRANCH_NAME
+read -p "Please Enter Commit Message: " MESSAGE
+read -p "Please Enter Branch Name: " BRANCH_NAME
+
 git add .
 git commit -m "$MESSAGE"
 git push origin "$BRANCH_NAME"
 
-print_message "Successfully pushing the changes"
+print_message "Successfully pushed the changes"
